f4be0586903be182fed400e952863daefdd56064
elihumetabox-glitch <elihu.metabox@gmail.com>
2026-09-04 15:50:58 +0400

    load real events

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import { EventDocument } from "@/database/event.model";
 import { cacheLife } from "next/cache";
-import events from "@/lib/constants";
+
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
@@ -10,12 +10,9 @@ const Page = async () => {
      'use cache';
      cacheLife('hours')
 
-    //const response = await fetch(`${BASE_URL}/api/events`);
-    //const {events} = await response.json();
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const {events} = await response.json();
 
-    /*if (!response.ok) {
-        throw new Error(`Failed to fetch events: ${response.status} ${response.statusText}`);
-    }*/
 
 
 
